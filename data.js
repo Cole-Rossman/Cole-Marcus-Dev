d94e99c96b3c8b2d1f1686be9b037500c9b530a2
Cole Rossman <colerossman99@gmail.com>
2022-01-19 17:13:41 -0800

    data.js progress

---
+++ data.js
@@ -0,0 +1,30 @@
+export const people = [
+    {
+        id: 1,
+        name: 'Shrek',
+        age: 40,
+        bio: 'Shrek is a green ogre,',
+        hobbies: ['onions', 'princesses', 'swamps'],
+    },
+    {
+        id: 2,
+        name: 'Fiona',
+        age: 30,
+        bio: 'Fiona is a green ogre princess.',
+        hobbies: ['singing', 'towers', 'fighting'],
+    },
+    {
+        id: 3,
+        name: 'Donkey',
+        age: 40,
+        bio: 'Donkey is a talking donkey.',
+        hobbies: ['sleeping', 'adventure', 'parfaits'],
+    },
+    {
+        id: 4,
+        name: 'Dragon',
+        age: 50,
+        bio: 'A fire-breathing dragon.',
+        hobbies: ['donkeys', 'fire', 'guarding'],
+    },
+];
